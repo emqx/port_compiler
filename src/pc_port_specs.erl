a94c4afc253ada8ffe3d8eebdaaffde5d8a0f390
JianBo He <heeejianbo@163.com>
2020-08-13 19:34:38 +0800

    Rebase to remote repo & Fix windows compiling error (#1)
    * fixes blt/port_compiler#51, bit/port_compiler#21
    
    * fix object names on win32

--- src/pc_port_specs.erl
+++ src/pc_port_specs.erl
@@ -159,6 +159,7 @@ get_port_spec(Config, OsType, {Arch, Target, Sources}) ->
     get_port_spec(Config, OsType, {Arch, Target, Sources, []});
 get_port_spec(Config, OsType, {_Arch, Target, Sources, Opts}) ->
     Env = try_and_create_env(Config),
+    ProjectRoot = rebar_state:dir(Config),
     SourceFiles =
         lists:flatmap(
           fun(Source) ->
@@ -174,8 +175,8 @@ get_port_spec(Config, OsType, {_Arch, Target, Sources, Opts}) ->
             true  -> cxx;
             false -> cc
         end,
-    Target1 = filename:join(rebar_state:dir(Config), Target),
-    ObjectFiles = [pc_util:replace_extension(O, ".o") || O <- SourceFiles],
+    Target1 = filename:join(ProjectRoot, Target),
+    ObjectFiles = [pc_util:replace_extension(O, object_file_ext()) || O <- SourceFiles],
     #spec{type      = pc_util:target_type(Target1),
           target    = coerce_extension(OsType, Target1),
           link_lang = LinkLang,
